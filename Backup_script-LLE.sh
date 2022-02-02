654d87671510bf18f3b037142c7e08e5fc788b34
vinodkumar501 <37996837+vinodkumar501@users.noreply.github.com>
2022-02-02 18:22:34 +0530

    Update Backup_script-LLE.sh

--- Backup_script-LLE.sh
+++ Backup_script-LLE.sh
@@ -9,7 +9,7 @@ pg_dump -h 33.110.11.23 -U keycloak keycloak > 33_110_11_23_keycloak_$(date +%d-
 
 tar -czf db_backup/db_backup_$(date +%d-%m-%y).tar.gz *.bak 
 
-find /root/db_backup  -type  f -name '*.gz' -mtime +7 -exec rm {} \;
+find /root/db_backup  -type  f -name '*.gz' -mtime +7 -exec rm {} \;                            #this will remove olderthan 7 daysfiles
 
 #Notification
 echo "Database Backup for LLE Completed successfully $(date +%d-%m-%y)"| mail -r"testing.gmail.com" -s "DB Backup for LLE" devteam@gmail.com qateam@gmail.com 
