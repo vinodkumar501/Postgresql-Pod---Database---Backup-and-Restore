cbfefb15fe9334d3a98087163f44e48683809973
vinodkumar501 <37996837+vinodkumar501@users.noreply.github.com>
2022-02-02 16:34:40 +0530

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -15,20 +15,3 @@ find /root/db_backup  -type  f -name '*.gz' -mtime +7 -exec rm {} \;
 echo "Database Backup for LLE Completed successfully $(date +%d-%m-%y)"| mail -r"testing.gmail.com" -s "DB Backup for LLE" devteam@gmail.com qateam@gmail.com 
 rm -rf *.bak
 
-##########################################################################################
-#COMPLETED script explanation
-
-
-#pg_dump -h 33.110.11.29 -U keycloak keycloak > 33_110_11_29_keycloak_$(date +%d-%m-%y).bak
-#O/P
-#33_110_11_29_keycloak_02-02-22.bak
-
-#pg_dump -h 33.110.11.23 -U keycloak keycloak > 33_110_11_23_keycloak_$(date +%d-%m-%y).bak
-#33_110_11_23_keycloak_02-02-22.bak
-
-#Compress all *.bak files into 1 file as tar.gz
-#tar -czf db_backup/db_backup_$(date +%d-%m-%y).tar.gz *.bak 
-#it takes all *.bak files created and create a tar.gz file under 
-
-#dbbackup/db_backup_02-02-22.tar.gz  (db_backup_02-02-22.tar.gz)
-
